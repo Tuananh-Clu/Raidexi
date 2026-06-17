c2b1c3164464d17a00fc379a6d8eceec2ead8382
Tuan Anh <yianh798@gmail.com>
2026-06-17 21:16:13 +0700

    fix: update sender email address in MailService to use a static email

--- Backend/Raidexi/Infrastructure/Services/MailService.cs
+++ Backend/Raidexi/Infrastructure/Services/MailService.cs
@@ -32,7 +32,7 @@ namespace Raidexi.Infrastructure.Services
             }
 
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Raidexi", mailAccount));
+            message.From.Add(new MailboxAddress("Raidexi", "raidexi@example.com"));
             message.To.Add(MailboxAddress.Parse(sendMailRequest.To));
             message.Subject = sendMailRequest.Subject;
 
